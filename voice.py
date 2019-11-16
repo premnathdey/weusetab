49090d09c868fd2d3afa8b50e4eff5a6b5387e6e
Satyam Kumar <io.satyamtg@gmail.com>
2019-11-17 04:11:11 +0530

    Update everything

--- voice.py
+++ voice.py
@@ -7,6 +7,8 @@ import requests
 import logging
 import time
 import json
+import subprocess
+import face_recognition
 
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
@@ -16,18 +18,7 @@ ask = Ask(app, '/')
 """ These functions handle what are essentially the beginning and end of the main use case of the skill."""
 @ask.launch
 def start_session():
-    while 1:
-        if (alexaready.fetchStatus('/captionready') == 1):
-            caption = fetchcaption.fetchCaption()
-            choice = [
-                "I'm glad that I can help you. I see",
-                "Glad to be of some help. There's",
-                "I see in front of you"
-            ]
-            alexaready.setStatus('captionready', 0)
-            return question(random.choice(choice)+' '+caption+' Is there anything else I can do for you?')
-        else:
-            return question("Please Wait. Processing...")
+    return question("Please Wait. Processing...")
 
 
 @ask.intent('DescribeWorldIntent')
@@ -38,34 +29,30 @@ def speaknext():
         "I see in front of you",
         "Now I can see "
     ]
-    files = {
-        'image': ('imgcptn/png/example.jpeg', open('imgcptn/png/example.jpeg', 'rb')),
-    }
-    response = requests.post(
-        'http://localhost:5000/model/predict', files=files)
-    json_data = json.loads(response.text)
-    print(json_data)
-    return question("Hello")
+    result = subprocess.getoutput(
+        'curl -F "image=@imgcptn/png/example.jpeg" -X POST http://localhost:5000/model/predict')
+    result = result.split('{', 1)[1]
+    result = "{" + result
+    res = json.loads(result)
+    speechstring = res["predictions"][0]["caption"]
+    return question(speechstring)
 
 
 @ask.intent('WantPersonNames')
 def speakpersonname():
-    if (alexaready.fetchStatus('/peopleready') == 1):
-        personnames = fetchperson.fetchCaption()
-
-        print(personnames)
-        if(personnames != '0'):
-            choice = [
-                "I see "
-            ]
-            print(personnames)
-            alexaready.setStatus('peopleready', 0)
-            return question(random.choice(choice)+' '+personnames+' You might want to say hi to them.')
-        else:
-            alexaready.setStatus('peopleready', 0)
-            return question("I am sorry but I believe that I do not recognize anyone here")
+    picture_of_me = face_recognition.load_image_file("me.jpeg")
+    my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+    # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+    unknown_picture = face_recognition.load_image_file("unknown.jpeg")
+    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+    # Now we can see the two face encodings are of the same person with `compare_faces`!
+    results = face_recognition.compare_faces(
+        [my_face_encoding], unknown_face_encoding)
+    if results[0] == True:
+        print("It's a picture of me!")
     else:
-        return question("Please Wait. Processing...")
+        print("It's not a picture of me!")
+    return question("Please Wait. Processing...")
 
 
 if __name__ == '__main__':
